1d1996c67ee9ef5840624332a15e6ff8def07283
Amir Fikri <amir_fikri@egxp.io>
2022-10-30 15:24:55 +0900

    Formatted with swiftformat

--- iOSEngineerCodeCheckTests/iOSEngineerCodeCheckTests.swift
+++ iOSEngineerCodeCheckTests/iOSEngineerCodeCheckTests.swift
@@ -6,11 +6,10 @@
 //  Copyright © 2020 YUMEMI Inc. All rights reserved.
 //
 
-import XCTest
 @testable import iOSEngineerCodeCheck
+import XCTest
 
 class iOSEngineerCodeCheckTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -30,5 +29,4 @@ class iOSEngineerCodeCheckTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
